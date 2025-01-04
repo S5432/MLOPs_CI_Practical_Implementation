c537736b1e0ca93b3f7b66caed7f06ea38b9ce8a
Subhash <sprajapat160599@gmail.com>
2025-01-04 23:14:19 +0530

    Add project to streamlite

---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+# Streamlit UI
+
+st.title("Power Calculator")
+st.write("Enter a number to calculate its square, cube, and fourth power.")
+
+# Get User Input
+
+n = st.number_input("Enter an integer", value = 1, step =1)
+
+# Calculate results
+
+square = n**2
+cube = n**3
+fourth_power = n**4
+
+# Display results
+
+st.write(f"The sqaure of {n} is: {square}")
+st.write(f"The cube of {n} is: {cube}")
+st.write(f"The fourth power of {n} is: {fourth_power}")
